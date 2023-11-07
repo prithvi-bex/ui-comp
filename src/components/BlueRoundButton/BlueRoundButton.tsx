dac5cfb227c6c56e679dec94339c94f40859f10b
prithvi-bex <pmatrawadiya@bexcard.co.uk>
2023-11-07 22:56:42 +0530

    feat(repo): setting up repo added dist updated * WORKING

---
+++ src/components/BlueRoundButton/BlueRoundButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BlueRoundButton = ({ lable, link }: { lable: string; link: string }) => {
+  return (
+    <div className="bg-blue font-vietnampro rounded-full px-9 py-2 text-sm font-medium text-white ">
+      <p>Join bex</p>
+    </div>
+  );
+};
+
+export default BlueRoundButton;
